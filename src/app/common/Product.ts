e193cea4334f006b847c90682f857c987223e544
dung <thuydung452k1@gmail.com>
2024-04-11 23:34:58 +0700

    first commit

--- src/app/common/Product.ts
+++ src/app/common/Product.ts
@@ -12,6 +12,7 @@ export class Product {
     'enteredDate': Date;
     'category': Category;
     // 'rates': Rate[];
+    priceNew: any
 
     constructor(productId:number) {
         this.productId = productId;
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -18,20 +18,20 @@ import { Product } from '../../common/Product';
 })
 export class HomePageComponent implements OnInit {
 
-  products!: Product[];
+  products!: any;
   productsF!: Product[];
   isLoading = true;
 
-  rates!:Rate[];
+  rates!: Rate[];
   rateProduct!: Rate[];
 
   customerId!: number;
-  user!:Customer;
+  user!: Customer;
 
   cart!: Cart;
   cartDetail!: CartDetail;
   cartDetails!: CartDetail[];
-  totalCartItem!:number;
+  totalCartItem!: number;
 
   totalLength!: number;
   page: number = 1;
@@ -40,12 +40,12 @@ export class HomePageComponent implements OnInit {
   keyF: string = '';
   reverse: boolean = true;
 
-  constructor(private productService: ProductService, private router: Router, private toastr: ToastrService, 
+  constructor(private productService: ProductService, private router: Router, private toastr: ToastrService,
     private cartService: CartService, private rateService: RateService, private localStorageService: LocalStorageService) {
   }
 
   ngOnInit(): void {
-    this.cartService.$data.subscribe(data=>{
+    this.cartService.$data.subscribe(data => {
       this.totalCartItem = data;
     })
     this.getAllRate();
@@ -55,7 +55,7 @@ export class HomePageComponent implements OnInit {
 
   getUser() {
     this.user = this.localStorageService.getUser();
-    if(this.user != null) {
+    if (this.user != null) {
       this.customerId = this.user.userId;
     }
   }
@@ -64,7 +64,15 @@ export class HomePageComponent implements OnInit {
     this.productService.getAll().subscribe(data => {
       this.isLoading = false;
       this.products = data as Product[];
-      this.productsF = this.products;
+      this.productsF = this.products.map((product: Product) => {
+        return {
+          ...product,
+          salePrice: product.price * (1 - product.discount / 100),
+          salePriceAsc: product.price * (1 - product.discount / 100)
+        }
+      })
+
+      console.log('eeeee', this.productsF)
       this.totalLength = this.products.length;
     }, error => {
       this.toastr.error('Lỗi truy xuất dữ liệu!', 'Hệ thống');
@@ -72,46 +80,46 @@ export class HomePageComponent implements OnInit {
   }
 
   getAllRate() {
-    this.rateService.getAll().subscribe(data=>{
+    this.rateService.getAll().subscribe(data => {
       this.rates = data as Rate[];
-    }, error=>{
-      this.toastr.error('Lỗi truy xuất dữ liệu! '+error.status, 'Hệ thống');
+    }, error => {
+      this.toastr.error('Lỗi truy xuất dữ liệu! ' + error.status, 'Hệ thống');
     })
   }
 
-  getStar(id:number):number {
+  getStar(id: number): number {
     //push la phai co 1 cai bien de ghi no moi duoc TT
-    var length:number;
+    var length: number;
     this.rateProduct = [];
     for (const item of this.rates) {
-      if(item.product.productId === id) {
+      if (item.product.productId === id) {
         length = this.rateProduct.push(item);
       }
     }
-    var star:number=0;
-    for(const item of this.rateProduct) {
-      star+=item.star;
+    var star: number = 0;
+    for (const item of this.rateProduct) {
+      star += item.star;
     }
-    if(this.rateProduct.length == 0) {
+    if (this.rateProduct.length == 0) {
       return 0;
     }
-    return star/this.rateProduct.length;
+    return star / this.rateProduct.length;
   }
 
   addCart(productId: number, price: number) {
     this.user = this.localStorageService.getUser();
-    if(this.user != null) {
+    if (this.user != null) {
       this.customerId = this.user.userId;
       this.cartService.getCart(this.customerId).subscribe(data => {
         this.cart = data as Cart;
-        this.cartDetail = new CartDetail(0,1,price,new Product(productId),new Cart(this.cart.id));
-        this.cartService.postDetail(this.cartDetail).subscribe(data=>{
+        this.cartDetail = new CartDetail(0, 1, price, new Product(productId), new Cart(this.cart.id));
+        this.cartService.postDetail(this.cartDetail).subscribe(data => {
           this.toastr.success('Thêm vào giỏ hàng thành công!', 'Hệ thống!');
-          this.cartService.getAllDetail(this.cart.id).subscribe(data=>{
+          this.cartService.getAllDetail(this.cart.id).subscribe(data => {
             this.cartDetails = data as CartDetail[];
             this.cartService.setData(this.cartDetails.length);
           })
-        }, error=>{
+        }, error => {
           this.toastr.error('Sản phẩm này có thể đã hết hàng!', 'Hệ thống');
           this.router.navigate(['/home-page']);
           window.location.href = "/home-page";
@@ -124,12 +132,16 @@ export class HomePageComponent implements OnInit {
     } else {
       this.router.navigate(['/login']);
     }
-    
+
   }
 
   search(event: any) {
     const fValue = (event.target as HTMLInputElement).value;
-    this.products = this.productsF.filter(p => p.name.toLowerCase().includes(fValue.toLowerCase()));
+    console.log("fValue", fValue);
+
+    this.products = this.productsF.filter((p: any) => p.name.toLowerCase().includes(fValue.toLowerCase()));
+    console.log(this.products);
+
     this.totalLength = this.products.length;
   }
 
@@ -137,19 +149,17 @@ export class HomePageComponent implements OnInit {
     if (keyF === 'enteredDate') {
       this.key = 'enteredDate';
       this.reverse = true;
-    } else
-      if (keyF === 'priceDesc') {
-        this.key = 'price';
-        this.reverse = true;
-      } else
-        if (keyF === 'priceAsc') {
-          this.key = 'price';
-          this.reverse = false;
-        }
-        else {
-          this.key = '';
-          this.reverse = true;
-        }
+    } else if (keyF === 'priceDesc') {
+      this.key = 'salePrice';
+      this.reverse = true;
+    } else if (keyF === 'priceAsc') {
+      this.key = 'salePriceAsc';
+      this.reverse = false;
+    }
+    else {
+      this.key = '';
+      this.reverse = true;
+    }
   }
 
 }
